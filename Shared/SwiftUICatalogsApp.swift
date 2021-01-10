a69749c3b18bc7dbac1a7219a4c8ef11e23d2fcd
daybysay <yukita.sekai+github@gmail.com>
2021-01-10 15:52:47 +0900

    Initial Commit

---
+++ Shared/SwiftUICatalogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICatalogsApp.swift
+//  Shared
+//
+//  Created by Sei Takayuki on 2021/01/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICatalogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
